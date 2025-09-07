68f9e31dda8fa9aa3d81544656940b01420efb63
Lorant Gulyas <gulyas.lorant@gmail.com>
2025-09-07 10:42:47 +0200

    Initial Update

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Lorant Lernt Bass</h1>
+    </div>
+  );
+}
+
+export default App;
